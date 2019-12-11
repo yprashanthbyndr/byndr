3c10e03a7a158017f047d1d305ac47f29c2f1632
Shaik Raheem <91a447raheem@gmail.com>
2019-12-11 17:43:10 +0530

    Updated Home Page and Navigation

--- src/theme/components/Navigation.tsx
+++ src/theme/components/Navigation.tsx
@@ -22,7 +22,7 @@ export default class Navigation extends React.Component<props, any> {
                     <div className="main_menu_inr">
                         <div className="menu_cancel">
                             <span style={{ float: 'right', cursor: 'pointer',color:"#fff" }} onClick={() => Open_Or_Close_MenuBar()}>
-                                <i className="material-icons">
+                                <i className="material-icons" style={{fontSize:"35px"}}>
                                     close
         </i>
                             </span>
@@ -51,29 +51,38 @@ export default class Navigation extends React.Component<props, any> {
 <div className="submenu">
     <ul>
     <li className="subMenuItem">
-    <NavLink exact to="/lms-for-teachers">For Teachers</NavLink>
+    <NavLink exact activeClassName="activeMenu" to="/lms-for-teachers" onClick={() =>   {
+                                            this.ScroolToTp();
+                                            Open_Or_Close_MenuBar()
+                                        }}>For Teachers</NavLink>
     </li>
     <li className="subMenuItem">
-    <NavLink exact to="/lms-for-students">For Students</NavLink>
+    <NavLink exact activeClassName="activeMenu" to="/lms-for-students" onClick={() =>   {
+                                            this.ScroolToTp();
+                                            Open_Or_Close_MenuBar()
+                                        }}>For Students</NavLink>
     </li>
     <li className="subMenuItem">
-    <NavLink exact to="/lms-for-admins">For Colleges</NavLink>
+    <NavLink exact activeClassName="activeMenu" to="/lms-for-admins" onClick={() =>   {
+                                            this.ScroolToTp();
+                                            Open_Or_Close_MenuBar()
+                                        }}>For Colleges</NavLink>
     </li>
     </ul>
 </div>
 
                                 </li>
-                                <li className="menuItem">
-                                    <a target="_blank" href="https://byndr.breezy.hr/">Careers <sup style={{fontSize: 12,
-color:"yellow"}}>We're hiring</sup></a>
-                                </li>
-                                <li className="menuItem">
-                                    <NavLink exact to="/blog"
+                                {/* <li className="menuItem">
+                                    <NavLink exact to="/pricing"
                                     activeClassName="activeMenu"
                                         onClick={() =>   {
                                             this.ScroolToTp();
                                             Open_Or_Close_MenuBar()
-                                        }}>Blog</NavLink>
+                                        }}>Pricing</NavLink>
+                                </li> */}
+                                <li className="menuItem">
+                                    <a target="_blank" href="https://byndr.breezy.hr/">Careers <sup style={{fontSize: 12,
+color:"yellow"}}>We're hiring</sup></a>
                                 </li>
                                 <li className="menuItem">
                                     <NavLink exact to="/contact-us"
@@ -83,27 +92,21 @@ color:"yellow"}}>We're hiring</sup></a>
                                             Open_Or_Close_MenuBar()
                                         }}>Contact Us</NavLink>
                                 </li>
-                                {/* <li className="submenu">
-                    <div className="menuItem">
-                    <NavLink exact to="/">Home</NavLink>
-                    </div>
-                    <div className="submenu">
-                        <ul>
-                            <li className="subMenuItem">
-                            <NavLink exact to="/">Home</NavLink>
-                            </li>
-                            <li className="subMenuItem">
-                            <NavLink exact to="/">Home</NavLink>
-                            </li>
-                            <li className="subMenuItem">
-                            <NavLink exact to="/">Home</NavLink>
-                            </li>
-                            <li className="subMenuItem">
-                            <NavLink exact to="/">Home</NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                </li> */}
+                                <li className="menuItem">
+                                    <NavLink exact to="/blog"
+                                    activeClassName="activeMenu"
+                                        onClick={() =>   {
+                                            this.ScroolToTp();
+                                            Open_Or_Close_MenuBar()
+                                        }}>Blog</NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="menu_cta_btns">
+                            <ul>
+                                <li><a href="./">Get Started For Free</a></li>
+                                <li><a href="./" style={{    background: "transparent",
+    border: "1px solid #ffffff",padding:"14px 0"}}>Request A Demo</a></li>
                             </ul>
                         </div>
                     </div>
