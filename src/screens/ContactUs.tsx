4c2a617ba6a10847bccc8f9658c148eadba0d4fb
Shaik Raheem <91a447raheem@gmail.com>
2019-12-13 15:09:35 +0530

    Remove Stickey For Contact Us page

--- src/screens/ContactUs.tsx
+++ src/screens/ContactUs.tsx
@@ -20,7 +20,7 @@ class ContactUs extends React.Component<props, any> {
 
         return (
             <div>
-                <div className="header_main_container"><Header /></div>
+                <Header />
                 <div className="bodySection">
                     <div className="contact_section">
                         <div className="contact_section_inr">
