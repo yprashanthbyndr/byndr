12b2dbbb92598043457e130c1d4eee76bd7acf40
Shaik Raheem <91a447raheem@gmail.com>
2019-12-20 13:57:30 +0530

    Minor Updates related to Request a demo page

--- src/screens/Request_demo.tsx
+++ src/screens/Request_demo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Seo } from '../theme/components';
 import { connect } from 'react-redux';
 import { SaveScroll_Height } from '../services';
-import { byndr_Logo_White, krishnaMurthi, Icon_Testimonial_pink } from '../assets';
+import { byndr_Logo_White, krishnaMurthi, Icon_Testimonial_pink, learn_innovate_together, PlanProgress, collaboration_communication, assess_support, peer_to_peer_learning, secure_technology } from '../assets';
 import { NavLink } from 'react-router-dom';
 
 interface props {
@@ -92,17 +92,98 @@ class RequestDemo extends React.Component<props, any> {
                                             <h4>What does Byndr do?</h4>
                                             <ul>
                                                 <li>
-                                                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut.
+                                                    <div className="requestDemoList">
+                                                        <div className="requestDemoListTitle">
+                                                            <span className="requestDemoListImage">
+                                                                <img src={learn_innovate_together}/>
+                                                                </span>
+                                                            <span className="requestDemoListText">
+                                                                Learn &amp; Innovate
+                                                            </span>
+                                                        </div>
+                                                        <div className="requestDemoListContent">
+                                                            Enables teachers to provide study materials, communicate effectively and encourage pro-active thinking among students
+                                                        </div>
+                                                    </div>
                                                 </li>
                                                 <li>
-                                                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut.
+                                                <div className="requestDemoList">
+                                                        <div className="requestDemoListTitle">
+                                                            <span className="requestDemoListImage">
+                                                                <img src={PlanProgress}/>
+                                                                </span>
+                                                            <span className="requestDemoListText">
+                                                                Plan, Perform & Progress
+                                                            </span>
+                                                        </div>
+                                                        <div className="requestDemoListContent">
+                                                            It enables the students to access course material, get their doubts clarified, take assessments and monitor the progress.
+                                                        </div>
+                                                    </div>
                                                 </li>
                                                 <li>
-                                                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut.
+                                                <div className="requestDemoList">
+                                                        <div className="requestDemoListTitle">
+                                                            <span className="requestDemoListImage">
+                                                                <img src={collaboration_communication}/>
+                                                                </span>
+                                                            <span className="requestDemoListText">
+                                                                Collaboration & Co-ordination
+                                                            </span>
+                                                        </div>
+                                                        <div className="requestDemoListContent">
+                                                        All course activity rolls up into a simple feed, no clicks, simple social learning, collaboration with real-time push notifications, keeping users engaged and connected.
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                <div className="requestDemoList">
+                                                        <div className="requestDemoListTitle">
+                                                            <span className="requestDemoListImage">
+                                                                <img src={assess_support}/>
+                                                                </span>
+                                                            <span className="requestDemoListText">
+                                                                Assess & Support
+                                                            </span>
+                                                        </div>
+                                                        <div className="requestDemoListContent">
+                                                        It enables the teachers to create assignments, track performance, evaluate for better learning outcomes.
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                <div className="requestDemoList">
+                                                        <div className="requestDemoListTitle">
+                                                            <span className="requestDemoListImage">
+                                                                <img src={peer_to_peer_learning}/>
+                                                                </span>
+                                                            <span className="requestDemoListText">
+                                                            Peer-to-Peer Learning
+                                                            </span>
+                                                        </div>
+                                                        <div className="requestDemoListContent">
+                                                        It enables the students to create their own study group, share notes, exchange ideas within the group and focus on self-development.
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                <div className="requestDemoList">
+                                                        <div className="requestDemoListTitle">
+                                                            <span className="requestDemoListImage">
+                                                                <img src={secure_technology}/>
+                                                                </span>
+                                                            <span className="requestDemoListText">
+                                                            Secure Technology
+                                                            </span>
+                                                        </div>
+                                                        <div className="requestDemoListContent">
+                                                        A secure cloud-based learning platform that enables easy integration, secure storage for documents and materials, monitor the engagement level, no set-up costs and no servers to maintain.
+                                                        </div>
+                                                    </div>
                                                 </li>
                                             </ul>
                                         </div>
-                                        <div className="requestDemoTestimonials">
+                                        {/* <div className="requestDemoTestimonials">
                                             <div className="requestDemoTestimonialsInr">
                                                 <div className="requestDemoTestimonials_Icon"><img src={Icon_Testimonial_pink} /></div>
                                                 <div className="requestDemoTestimonials_Content">
@@ -122,7 +203,7 @@ class RequestDemo extends React.Component<props, any> {
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
+                                        </div> */}
                                     </div>
                                 </div>
                             </div>
@@ -202,7 +283,7 @@ class RequestDemo extends React.Component<props, any> {
                                                     <div className="requestdemo_singleRowField">
                                                         <div className="requestdemo_singleRowField">
                                                             <div className="requestdemo_singleRowField">
-                                                                <select value={this.state.sizeOfInstitue} onChange={(txt)=> this.StoreIPValues("sizeOfInstitue", txt.target.value)} className="disableField">
+                                                                <select value={this.state.sizeOfInstitue} onChange={(txt)=> this.StoreIPValues("sizeOfInstitue", txt.target.value)}>
                                                                     <option value="" selected>Select</option>
                                                                     <option value="0-25">0-25</option>
                                                                     <option value="25-50">25-50</option>
